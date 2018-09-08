a19fa747f4f3b3c82944e8d39e4b675df135dbc4
Juliet Urbina <julieturbina8@gmail.com>
2018-09-08 02:22:19 -0400

    authentication added

--- app.js
+++ app.js
@@ -8,23 +8,38 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+// const cors         = require('cors');
+const session    = require('express-session');
+const passport   = require('passport');
 
 
 mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/event-server', {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log('Connected to Mongo!');
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const passportSetup = require('./config/passport');
+passportSetup(passport);
+
 
 const app = express();
 
 // Middleware Setup
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,22 +52,22 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
+const authApi = require('./routes/auth-api');
 app.use('/', index);
-
+app.use('/', authApi);
+// default route is nothing else is caught
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 module.exports = app;
